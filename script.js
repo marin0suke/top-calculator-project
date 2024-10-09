e56b8e88c8cacb79785547ca68db012e200a70fa
marin0suke <110573250+marin0suke@users.noreply.github.com>
2024-10-09 10:29:36 +1000

    Add basic functions

---
+++ script.js
@@ -0,0 +1,19 @@
+let leftOperand;
+let rightOperand;
+
+function add(leftOperand, rightOperand) {
+    return leftOperand + rightOperand;
+};
+
+function subtract(leftOperand, rightOperand) {
+    return leftOperand - rightOperand;
+}
+
+function multiply(leftOperand, rightOperand) {
+    return leftOperand * rightOperand;
+}
+
+function divide(leftOperand, rightOperand) {
+    return leftOperand / rightOperand;
+}
+
