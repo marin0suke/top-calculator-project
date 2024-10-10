7369992477deff591cce5090b757a23762a01d85
marin0suke <110573250+marin0suke@users.noreply.github.com>
2024-10-10 12:17:48 +1000

    Add function for operand buttons, add displayValue and displayElement. Connect to html.

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let leftOperand;
+let leftOperand = document.querySelector(".")
 let rightOperand;
 let operator;
+
+let displayValue = ""; //
+
+const displayElement = document.querySelector(".display"); // connects to display html.
+const operands = document.querySelectorAll(".operand"); // get all operands.
+
+operands.forEach(operand => { // gets number from button 
+    operand.addEventListener("click", () => {
+        const buttonValue = operand.textContent;
+        displayValue += buttonValue; // concat it.
+        displayElement.textContent = displayValue; // displayElement is what shows up on screen - equal it to displayValue so it shows up. 
+    });
+});
+
+
 
 function operate(leftOperand, operator, rightOperand) {
     switch (operator) {
